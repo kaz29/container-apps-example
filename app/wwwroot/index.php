abe759f096c43ba2d43cdf51a3b12e78138d373e
Kaz WATANABE <kaz@decr.jp>
2024-04-06 17:28:53 +0900

    fix app

--- app/wwwroot/index.php
+++ app/wwwroot/index.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-$version = @file_get_contents(dirname(__DIR__) . '/VERSION');
+$version = @file_get_contents(__DIR__ . '/VERSION');
 $version = $version ? trim($version) : 'UNKNOWN';
 echo "Hello World! ($version)<br />\n";
 
